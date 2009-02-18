083e4d8383aa15822d57d347c13f354c4733add6
Ping Yin <pkufranky@gmail.com>
2009-02-18 16:18:14 +0800

    agent.sh: Initial add

---
+++ agent.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+USAGE="
+. agent.sh save|restore
+"
+
+auth_sock_file="$HOME/.ssh/SSH_AUTH_SOCK"
+case "$1" in
+save)
+	echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > $auth_sock_file
+	;;
+restore)
+	. $auth_sock_file
+	;;
+*)
+	echo "$USAGE"
+	;;
+esac
+
+
