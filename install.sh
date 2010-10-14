759cbff6a6eaf60b8fb1c3fc57aab22333193764
Ping Yin <pkufranky@gmail.com>
2010-10-14 10:40:10 +0800

    Add global default gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-for n in bashrc bash_profile inputrc screenrc lftp gitconfig
+for n in bashrc bash_profile inputrc screenrc lftp gitconfig gitignore
 do
 	ln -sfv -T .rc/$n .$n
 done
